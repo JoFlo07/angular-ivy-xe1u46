0f4f3712400fe22db330beaa145bdc069c8a25c1
JoFlo07 <florian.jost89@gmail.com>
2021-04-16 12:12:20 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,66 +18,23 @@ export class AppComponent implements OnInit {
     this.chart = Highcharts.chart("container", this.options);
   }
 
-  public options: Highcharts.Options = {
+  options: Highcharts.Options = {
     chart: {
-      height: 600,
+      borderRadius: 4,
       plotBackgroundColor: {
-        linearGradient: { x1: 1, x2: 0, y1: 0, y2: 1 },
-        stops: [[0.2, "#e3463b"], [0.5, "#ffda05"], [0.8, "#19cf1f"]]
+        linearGradient: { x1: 0, x2: 1, y1: 1, y2: 1 },
+        stops: [[0, "#19cf1f"], [0.5, "#ffda05"], [0.8, "#e3463b"]]
       }
     },
-    plotOptions: {
-      series: {
-        marker: {
-          lineWidth: 2,
-          fillOpacity: 1,
-          lineColor: "#ffffff",
-          states: {
-            hover: {
-              fillColor: "#ffffff"
-            }
-          }
-        },
-        dataLabels: {
-          enabled: true,
-          inside: true
-        }
-      }
-    },
-    title: {
-      text: "Risk Rating",
-      align: "left"
-    },
+    title: null,
     credits: {
       enabled: false
     },
-    tooltip: {
-      backgroundColor: "rgba(255,255,255,1)",
-      pointFormatter: function() {
-        return (
-          "<strong>" +
-          this.options.custom.riskTitle +
-          "</strong> </br></br> Exposure </br>" +
-          this.options.custom.exposure +
-          "</br> </br> Security Level </br>" +
-          this.x +
-          " %"
-        );
-      },
-      distance: 30
-    },
     yAxis: {
-      visible: false,
-      min: -0.5,
-      max: 4.5,
-      tickInterval: 0.5,
-      opposite: true
-    },
-    xAxis: {
       tickWidth: 1,
       reversed: true,
-      min: -5,
-      max: 105,
+      min: 0,
+      max: 100,
       tickInterval: 10,
       labels: {
         formatter: function() {
@@ -85,148 +42,37 @@ export class AppComponent implements OnInit {
         }
       }
     },
+    xAxis: {
+      tickWidth: 1,
+      reversed: true,
+      min: 0,
+      max: 13,
+      tickInterval: 1,
+      categories: isos
+    },
     series: [
       {
-        type: "bubble",
-        name: "",
-        minSize: 55,
-        maxSize: 55,
-        color: "transparent",
-        showInLegend: false,
-        dataLabels: [
-          {
-            align: "center",
-            verticalAlign: "top",
-            y: -25,
-            allowOverlap: true,
-            format: "{point.options.custom.riskType}",
-            style: {
-              color: "#ffffff",
-              textOutline: null,
-              fontSize: 15 + "px"
-            }
-          },
-          {
-            className: "center-label",
-            align: "center",
-            allowOverlap: true,
-            style: {
-              color: "#ffffff",
-              textOutline: null,
-              fontSize: 16 + "px"
-            },
-
-            formatter: function() {
-              return Highcharts.numberFormat(this.y, 1);
-            }
-          }
-        ],
-        data: [
-          {
-            custom: {
-              riskType: "BI",
-              riskTitle: "Business Interruption",
-              exposure: "very-low to low"
-            },
-            x: 10,
-            y: 4.0,
-            events: {
-              mouseOver: this.handleMouseOver,
-              mouseOut: this.handleMouseOut
-            }
-          },
-          {
-            custom: {
-              riskType: "DB",
-              riskTitle: "Data Breach",
-              exposure: "very-low"
-            },
-            x: 25,
-            y: 2.1,
-            events: {
-              mouseOver: this.handleMouseOver,
-              mouseOut: this.handleMouseOut
-            }
-          },
-          {
-            custom: {
-              riskType: "OV",
-              riskTitle: "Overall",
-              exposure: "low"
-            },
-            x: 0,
-            y: 0,
-            events: {
-              mouseOver: this.handleMouseOver,
-              mouseOut: this.handleMouseOut
-            }
-          }
-        ]
+        type: "bar",
+        color: "#0000004f",
+        showInLegend: false
       }
-    ],
-    responsive: {
-      rules: [
-        {
-          condition: {
-            maxWidth: 400
-          },
-          chartOptions: {
-            series: [
-              {
-                type: "bubble",
-                minSize: 35,
-                maxSize: 35
-              }
-            ],
-            plotOptions: {
-              series: {
-                dataLabels: {
-                  style: {
-                    fontSize: 13 + "px"
-                  }
-                }
-              }
-            }
-          }
-        }
-      ]
-    }
+    ]
   };
-
-  update() {
-    this.chart.series[0].data.forEach(d => {
-      //TODO update matching bubble
-      d.update({
-        x: 10,
-        y: 2,
-        custom: {
-          riskType: "OV",
-          riskTitle: "Overall",
-          exposure: "high"
-        }
-      });
-    });
-  }
-
-  handleMouseOver(event) {
-    //TODO: cheeck event type
-    const s = event.target as any;
-    event.target.dataLabels.forEach(label => {
-      if (label.options.className === "center-label") {
-        label.css({
-          color: "#30302E"
-        });
-      }
-    });
-  }
-
-  handleMouseOut(event) {
-    //TODO: cheeck event type
-    const s = event.target as any;
-    s.dataLabels.forEach(label => {
-      label.css({
-        color: "#ffffff"
-      });
-    });
-  }
 }
+
+const isos = [
+  "A.05 Information security policies",
+  "A.06 Organization of information security",
+  "A.07 Human resource security",
+  "A.08 Asset management",
+  "A.09 Access control",
+  "A.10 Cryptography",
+  "A.11 Physical and environmental security",
+  "A.12 Operations security",
+  "A.13 Communications security",
+  "A.14 System acquisition, development and maintenance",
+  "A.15 Supplier relationships",
+  "A.16 Information security incident management",
+  "A.17 Information security aspects of BCM",
+  "A.18 Compliance"
+];
